cb61ee578c00448f0e6b84af11b26bdafef9e07a
KelvinSyahriReza <kelvin30.evin@yahoo.com>
2018-10-25 17:39:45 +0700

    'new'

---
+++ Week 2/Switchcase.js
@@ -0,0 +1,31 @@
+// Directions
+// Kamu akan diberikan sebuah tanggal dalam tiga variabel, yaitu hari, bulan, dan tahun. Disini kamu diminta untuk membuat format tanggal. Misal tanggal yang diberikan adalah hari 1, bulan 5, dan tahun 1945. Maka, output yang harus kamu proses adalah menjadi 1 Mei 1945.
+
+// Gunakan switch case untuk kasus ini!
+
+// Contoh:
+
+// var hari = 21; var bulan = 1; var tahun = 1945;
+
+// Maka hasil yang akan tampil di console adalah: '21 Januari 1945';
+
+var tanggal = 21 ;
+var bulan = 1 ;
+var tahun = 1945;
+  
+switch (bulan) {
+  case 1  : {console.log(tanggal + ' Januari '  + tahun) ; break; }
+  case 2  : {console.log(tanggal + ' Februari ' + tahun) ; break; }
+  case 3  : {console.log(tanggal + ' Maret '    + tahun) ; break; }
+  case 4  : {console.log(tanggal + ' April '    + tahun) ; break; }
+  case 5  : {console.log(tanggal + ' Mei '      + tahun) ; break; }
+  case 6  : {console.log(tanggal + ' Juni '     + tahun) ; break; }
+  case 7  : {console.log(tanggal + ' Juli '     + tahun) ; break; }
+  case 8  : {console.log(tanggal + ' Agustus '  + tahun) ; break; }
+  case 9  : {console.log(tanggal + ' September '+ tahun) ; break; }
+  case 10 : {console.log(tanggal + ' Oktober '  + tahun) ; break; }
+  case 11 : {console.log(tanggal + ' November ' + tahun) ; break; }
+  case 12 : {console.log(tanggal + ' Desember ' + tahun ) ; break; }
+  default : {console.log('Pilih Tanggal')}
+  
+}
