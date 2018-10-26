a4ef98ad61fe263dfbe49288bf6319f95209f1d9
KelvinSyahriReza <kelvin30.evin@yahoo.com>
2018-10-27 05:22:01 +0700

    'new'

---
+++ Week 2/Function.js
@@ -0,0 +1,18 @@
+function shoutout() {
+    console.log('Hallo Function !')
+}
+
+shoutout ()
+
+console.log('<<<<<<<<<<<<<<<<<================>>>>>>>>>>>>>>')
+
+var num1 = 5
+var num2 = 6
+
+function tampilkanAngka(num1, num2) {
+  return num1 * num2
+}
+
+console.log(tampilkanAngka(num1, num2))
+
+console.log('<<<<<<<<<<<<<<<<<================>>>>>>>>>>>>>>')
