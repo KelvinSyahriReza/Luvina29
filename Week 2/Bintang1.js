97b03e30c3288c09c9aea3b460b30c911abc97e1
KelvinSyahriReza <kelvin30.evin@yahoo.com>
2018-10-26 14:41:10 +0700

    'New'

--- Week 2/Bintang1.js
+++ Week 2/Bintang1.js
@@ -1,5 +1,25 @@
 var bintang = ''
 
-for (var i= 0; i < 5; i++){
+for (var i = 0; i < 5; i++){
   console.log('*')
+}
+
+console.log('<<<<<<<<<<<<<<=================>>>>>>>>>>>>')
+
+var bintang = ''
+for (var i= 0; i < 5; i++){
+  var tampung = ''
+  for (var j =0; j < 5; j++) {
+    tampung += '*'
+  }
+  console.log(tampung)
+}
+
+console.log('<<<<<<<<<<<<<<=================>>>>>>>>>>>>')
+
+var tampung = ''
+for (var i= 0; i < 5; i++){
+    tampung += '*'
+ 
+  console.log(tampung)
 }
